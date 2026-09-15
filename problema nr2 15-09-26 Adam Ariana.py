4977c5584dc52d801dda4cd89a4317b1dd4219e3
Adam Ariana <adamariana373@gmail.com>
2026-09-15 10:17:00 +0300

    Add files via upload

---
+++ problema nr2 15-09-26 Adam Ariana.py
@@ -0,0 +1,7 @@
+a=5
+b=3
+print('Suma','a', '+', 'b', '=', a+b, end='\n')
+print ('Produsul', a, '*', b, '=', a*b, end='\n')
+print ('Diferenta', a, '-', b, '=', a-b, end='\n')
+print ('Catul impartirii', a, '//', b,'=', a//b, end='\n')
+print ('Restul', a, '%', b,'=', a%b, end='\n')
