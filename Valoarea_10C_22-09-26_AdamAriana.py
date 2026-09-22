9dbd54d411abffdaf2a8547e365a5900276f0854
Adam Ariana <adamariana373@gmail.com>
2026-09-22 10:17:49 +0300

    Add files via upload

---
+++ Valoarea_10C_22-09-26_AdamAriana.py
@@ -0,0 +1,8 @@
+import math
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+print ('Numerele introduse sunt:', 'a', 'b', 'c')
+print ('Valoareea maxima este:', max (a, b, c))
+print ('Valoareea maxima este:', min (a, b, c))
+print ('Valoareea absoluta dintre', a, 'si', b, 'este', abs(a-b) )
